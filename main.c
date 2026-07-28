943aa5b4840084c31d18d164f477d4ec736d70ae
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.ph>
2026-07-28 17:48:29 +0800

    minor fixes

--- main.c
+++ main.c
@@ -12,10 +12,10 @@ int main(void)
     asmhello();
 
     //test values
-    float X1[] = { 1.5f, 4.0f, 3.5f, 2.0f };
-    float X2[] = { 3.0f, 2.5f, 2.5f, 1.0f };
-    float Y1[] = { 4.0f, 3.0f, 3.5f, 3.0f };
-    float Y2[] = { 2.0f, 2.5f, 1.0f, 1.5f };
+    float X1[] = {1.5f, 4.0f, 3.5f, 2.0f};
+    float X2[] = {3.0f, 2.5f, 2.5f, 1.0f};
+    float Y1[] = {4.0f, 3.0f, 3.5f, 3.0f};
+    float Y2[] = {2.0f, 2.5f, 1.0f, 1.5f};
 
     float Z[4];
 
@@ -37,6 +37,6 @@ void distance_kernel(const float* X1, const float* X2, const float* Y1, const fl
         float dx = X2[i] - X1[i];
         float dy = Y2[i] - Y1[i];
 
-        Z[i] = sqrtf(dx * dx + dy * dy);
+        Z[i] = sqrtf((dx * dx) + (dy * dy));
     }
 }
