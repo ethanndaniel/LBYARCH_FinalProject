c39ca30ee978aee90c533a37cdd0a4ff6ef613bb
unknown <juantitusr@gmail.com>
2026-08-07 08:40:10 +0800

    Updated the assmebly file to have the computations for the kernel

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,14 +1,38 @@
-; assembly part using x86-64
-section .data
-msg db "Hello World",13,10,0
+%include "io64.inc"
+
 section .text
 bits 64
-default rel ; to handle address relocation
-global asmhello
-extern printf
-asmhello:
-sub rsp, 8*5 ; caller
-lea rcx, [msg]
-call printf
-add rsp, 8*5
-ret
+default rel
+
+global asmkernel
+asmkernel:
+    ; retrieve 5th and 6th arguments from the stack
+    mov r10, [rsp + 40]
+    mov r11d, [rsp + 48]
+    xor rax, rax
+.loop:
+    cmp rax, r11
+    jge .done
+    
+    ; compute dx = X2[i] - X1[i] and square it (dx * dx)
+    movss xmm0, [rdx + rax*4]
+    subss xmm0, [rcx + rax*4]
+    mulss xmm0, xmm0
+
+    ; compute dy = Y2[i] - Y1[i] and square it (dy * dy)
+    movss xmm1, [r9 + rax*4]
+    subss xmm1, [r8 + rax*4]
+    mulss xmm1, xmm1
+
+    ; sum squared differences and compute square root
+    addss xmm0, xmm1
+    sqrtss xmm0, xmm0
+
+    ; store result in Z[i]
+    movss [r10 + rax*4], xmm
+
+    inc rax
+    jmp .loop
+
+.done:
+    ret
